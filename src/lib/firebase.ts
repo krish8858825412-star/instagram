54c2e035288591a6735106b677b3e987e68b00ec
Krishna Jaiswal <krishnajaiswal8858825412@gmail.com>
2025-12-14 06:44:04 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1rFDsXNwqnOrawI4S_GxbGPOwDmeAkYE",
-  authDomain: "spare-study.firebaseapp.com",
-  databaseURL: "https://spare-study-default-rtdb.firebaseio.com",
-  projectId: "spare-study",
-  storageBucket: "spare-study.firebasestorage.app",
-  messagingSenderId: "729648401813",
-  appId: "1:729648401813:web:bd6daf4f54133d74c1fa9c"
+  apiKey: "AIzaSyA5o-FeeN_CdaTJQC9bp15kpwlVZBheCcE",
+  authDomain: "instagram-d7fcb.firebaseapp.com",
+  projectId: "instagram-d7fcb",
+  storageBucket: "instagram-d7fcb.firebasestorage.app",
+  messagingSenderId: "334000887042",
+  appId: "1:334000887042:web:d0318720d0bead5f2cde1b"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
